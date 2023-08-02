36ffb09f2dda0e88158e731692c7eef424e3bd67
Vitalii Sili <vitaliisili@yahoo.com>
2023-08-02 18:53:27 +0200

    ♻️ Refactor code. Add standard function for display table in console

--- src/util.py
+++ src/util.py
@@ -1,3 +1,11 @@
 from pathlib import Path
+from typing import List, Any
+
+from tabulate import tabulate
 
 ROOT_DIR = Path(__file__).parent.parent
+
+
+def display_table(table_headers: List[str], data: List[Any]):
+    table = [tab for tab in data]
+    print(tabulate(table, headers=table_headers, tablefmt='fancy_grid'))
