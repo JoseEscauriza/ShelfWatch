5d94747650accf829f58b705676a85b8874954ab
Jose Escauriza <escaurizajose@gmail.com>
2023-08-04 10:45:31 +0200

    Added method for inserting data into database as well as necessary dependent methods and tests, as well as a couple bug fixes/feature implementations (menu options to go back/exit)

--- src/service.py
+++ src/service.py
@@ -1,5 +1,7 @@
 from psycopg import Connection
 from src.database import Database
+from src.custom_types import AuthorRow, StatusRow, BookRow
+from typing import Union
 
 
 class Service:
@@ -61,3 +63,68 @@ class Service:
             cursor.execute(query, (f"%{title}%",))
             result = cursor.fetchall()
             return result
+
+    def dm_insert_data(self, table: str, data: Union[str, AuthorRow, StatusRow, BookRow]):
+        if table == 'member':
+            query = """
+                INSERT INTO member
+                VALUES (default, %s) RETURNING id;
+            """
+
+            with self.db.cursor() as cursor:
+                cursor.execute(query, (data, ))
+                result = cursor.fetchone()[0]
+                self.db.commit()
+                return result
+
+        if table == 'book':
+            query = """
+                INSERT INTO book
+                VALUES (default, %s, %s, %s, %s) RETURNING id;
+            """
+
+            with self.db.cursor() as cursor:
+                cursor.execute(query, tuple(data.values()))
+                result = cursor.fetchone()[0]
+                self.db.commit()
+                return result
+
+        if table == 'author':
+            query = """
+                INSERT INTO author
+                VALUES (default, %s, %s, %s) RETURNING id;
+            """
+
+            with self.db.cursor() as cursor:
+                cursor.execute(query, tuple(data.values()))
+                result = cursor.fetchone()[0]
+                self.db.commit()
+                return result
+
+    def dm_prompt_member_data(self):
+        member_name = input("Please insert a username to store in the database: ")
+        return member_name
+
+    def dm_prompt_author_data(self):
+        author_first_name = input("Author's first name: ")
+        author_last_name = input("Author's last name: ")
+        author_birthdate = input("Author's birthdate: ")
+
+        return {
+            'author_first_name': author_first_name if author_first_name else None,
+            'author_last_name': author_last_name,
+            'author_birthdate': author_birthdate if author_birthdate else None
+        }
+
+    def dm_prompt_book_data(self):
+        author_id = input("Please insert the author's ID: ")
+        title = input("Please insert the book's title: ")
+        description = input("Please enter the book's short description: ")
+        genre = input("Please enter the book's genre: ")
+
+        return {
+            'author_id': int(author_id) if author_id else None,
+            'title': title,
+            'description': description if description else None,
+            'genre': genre
+        }
